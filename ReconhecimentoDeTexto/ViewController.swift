6c61c53cbc641e474467b21a8216c3c215e7b2d6
Pernas <bslucena@gmail.com>
2018-04-27 15:44:22 +0200

    Initial Commit

---
+++ ReconhecimentoDeTexto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReconhecimentoDeTexto
+//
+//  Created by Bernardo Sarto de Lucena on 4/27/18.
+//  Copyright © 2018 Bernardo Sarto de Lucena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
